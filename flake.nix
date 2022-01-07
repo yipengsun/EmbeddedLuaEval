ed4df66478797ee75517fedcdec7c3c43913af56
Yipeng Sun <syp@umd.edu>
2022-01-07 01:56:11 +0100

    Basic project setup.

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A simple demo to embed Lua in a C++ program.";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          name = "EmbededLuaEval";
+          buildInputs = with pkgs; [
+            lua
+            libyamlcpp
+          ];
+        };
+      });
+}
