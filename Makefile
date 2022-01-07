fcff98a207749abe41cd31fe0d62685e34e44d70
Yipeng Sun <syp@umd.edu>
2022-01-07 23:47:26 +0100

    Allow vector cli args; also show the power of embeded Lua.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 # Author: Yipeng Sun
-# Last Change: Fri Jan 07, 2022 at 10:51 PM +0100
+# Last Change: Fri Jan 07, 2022 at 11:46 PM +0100
 
 VPATH := include:src:bin
 HEADERS := $(shell find ./include -name "*.h")
@@ -9,18 +9,14 @@ COMPILER	:=	${CXX}
 CXXFLAGS	:=	-pthread -std=c++17 -m64 -O2 -march=native -mtune=native -Iinclude
 LINKFLAGS	:=	-llua -lyaml-cpp
 
-.PHONY: all clean run-demo
+.PHONY: clean run-demo
 
-all: clean
+run-demo: demo params/simple.yml
+	bin/demo -g area,r,name -t double,double,string -i ./params/simple.yml
+	bin/demo -g isCircle,wtf1,wtf2 -t bool,double,double -i ./params/simple.yml
 
 clean:
 	@rm -rf ./bin/*
-
-run-demo: demo params/simple.yml
-	bin/demo -g "area" -i ./params/simple.yml
-	bin/demo -g "r" -i ./params/simple.yml -t int
-	bin/demo -g "name" -i ./params/simple.yml -t string
-	bin/demo -g "isCircle" -i ./params/simple.yml -t bool
 
 %: %.cpp flake.nix $(HEADERS)
 	$(COMPILER) $(CXXFLAGS) -o bin/$@ $< $(LINKFLAGS)
--- src/demo.cpp
+++ src/demo.cpp
@@ -1,6 +1,6 @@
 // Author: Yipeng Sun
 // License: BSD 2-clause
-// Last Change: Fri Jan 07, 2022 at 10:39 PM +0100
+// Last Change: Fri Jan 07, 2022 at 11:47 PM +0100
 
 #include <cxxopts.hpp>
 #include <iostream>
@@ -9,16 +9,39 @@
 
 using namespace std;
 
+void printParamVal(ParamEval* const exe, const vector<string>& names,
+                   const vector<string>& types) {
+  for (int i = 0; i < names.size(); i++) {
+    auto paramType = types[i];
+    cout << "Parameter "
+         << "\"" << names[i] << "\"";
+    if (paramType == "double") {
+      auto val = exe->get<double>(names[i]);
+      cout << " = " << val << endl;
+    } else if (paramType == "int") {
+      auto val = exe->get<int>(names[i]);
+      cout << " = " << val << endl;
+    } else if (paramType == "bool") {
+      auto val = exe->get<bool>(names[i]);
+      cout << " = " << val << endl;
+    } else if (paramType == "string") {
+      auto val = exe->get<string>(names[i]);
+      cout << " = " << val << endl;
+    } else {
+      cout << " has an unsupported type: " << paramType << endl;
+    }
+  }
+}
+
 int main(int argc, char** argv) {
   cxxopts::Options argOpts("demo", "a demo on embedding Lua in C++.");
 
   // clang-format off
   argOpts.add_options()
     ("h,help", "print usage")
-    ("g,get", "get value of a known variable", cxxopts::value<string>())
+    ("g,get", "get value of variables", cxxopts::value<vector<string>>())
+    ("t,type", "variable types", cxxopts::value<vector<string>>())
     ("i,inputYml", "input YAML file", cxxopts::value<string>())
-    ("t,type", "parameter type", cxxopts::value<string>()
-     ->default_value("double"))
   ;
   // clang-format on
 
@@ -31,22 +54,9 @@ int main(int argc, char** argv) {
   auto exe = make_unique<ParamEval>();
   exe->loadParam(parsedArgs["inputYml"].as<string>());
 
-  auto paramType = parsedArgs["type"].as<string>();
-  if (paramType == "double") {
-    auto val = exe->get<double>(parsedArgs["get"].as<string>());
-    cout << "Parameter = " << val << endl;
-  } else if (paramType == "int") {
-    auto val = exe->get<int>(parsedArgs["get"].as<string>());
-    cout << "Parameter = " << val << endl;
-  } else if (paramType == "bool") {
-    auto val = exe->get<bool>(parsedArgs["get"].as<string>());
-    cout << "Parameter = " << val << endl;
-  } else if (paramType == "string") {
-    auto val = exe->get<string>(parsedArgs["get"].as<string>());
-    cout << "Parameter = " << val << endl;
-  } else {
-    cout << "Unsupported type: " << paramType << endl;
-  }
+  const auto names = parsedArgs["get"].as<vector<string>>();
+  const auto types = parsedArgs["type"].as<vector<string>>();
+  printParamVal(exe.get(), names, types);
 
   return 0;
 }
